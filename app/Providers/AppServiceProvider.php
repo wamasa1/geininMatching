df9e0542ac0ed9ed1269461a06518ff93366337b
masataka_iMac <cdehilmnrr@gmail.com>
2019-06-06 19:42:55 +0900

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      //herokuでhttpsにするため
+      if (\App::environment('production')) {
+      \URL::forceScheme('https');
+      }
     }
 }
