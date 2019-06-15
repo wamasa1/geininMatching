4dd9223975e8759e9121269b37b7fc931642797f
masataka_iMac <cdehilmnrr@gmail.com>
2019-06-15 19:08:58 +0900

    AccessTest&DatabaseTest追加

--- resources/views/layouts/matching.blade.php
+++ resources/views/layouts/matching.blade.php
@@ -55,6 +55,9 @@
 
       @yield('body')
     </div>
+
+    @yield('GoogleMap')
+
     <!-- twitter -->
     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
@@ -62,38 +65,5 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-    <!-- Google Map API関連（jQuery,js等） -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCxbeYuC7fA8pnAHbvOlxeAlZScWYYmCxE"></script>
-    <script type="text/javascript">
-      //NSC東京
-      var center1 = {
-        lat: 35.695036, // 緯度
-        lng: 139.760981 // 経度
-      };
-      var nsc_map = new google.maps.Map(document.getElementById('nsc_map'), {
-        center: center1,
-        zoom: 16 //地図のズームを設定
-      });
-      var marker = new google.maps.Marker({
-        position: center1, // マーカーを立てる位置を指定
-        map: nsc_map // マーカーを立てる地図を指定
-      });
-
-      //ワタナベコメディスクール
-      var center2 = {
-        lat: 35.647305,
-        lng: 139.694671
-      };
-      var watanabe_map = new google.maps.Map(document.getElementById('watanabe_map'), {
-        center: center2,
-        zoom: 16
-      });
-      var marker = new google.maps.Marker({
-        position: center2,
-        map: watanabe_map
-      });
-    </script>
   </body>
 </html>
