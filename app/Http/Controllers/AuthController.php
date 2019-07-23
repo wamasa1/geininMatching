fbc1b95f6392a4a4e1d8994399b92acdf96a6b63
masataka_iMac <cdehilmnrr@gmail.com>
2019-07-24 01:52:25 +0900

    検索画面リダイレクト修正

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,15 +16,13 @@ class AuthController extends Controller
   { //テストユーザーでログイン
     if ($request->test_user_id == 1) {
       Auth::guard('geinin')->loginUsingId(1);
-      
-      return redirect()->intended('/')->with('login', 'ログインしました');
+      return redirect()->intended('/search')->with('login', 'ログインしました');
     }
-
     //認証 email passwordの照合
     $credentials = $request->only('email', 'password');
 
     if (Auth::guard('geinin')->attempt($credentials)) {
-      return redirect()->intended('/')->with('login', 'ログインしました');
+      return redirect()->intended('/search')->with('login', 'ログインしました');
     } else {
       return redirect('/login')->with('login_failure', 'ログインに失敗しました');
     }
@@ -33,7 +31,6 @@ class AuthController extends Controller
   public function logout()
   {
     Auth::guard('geinin')->logout();
-
     return redirect('/')->with('logout', 'ログアウトしました');
   }
 }
