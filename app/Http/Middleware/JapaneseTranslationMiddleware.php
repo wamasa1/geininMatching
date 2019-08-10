e4d3dfb904078084f2c162b133cf5949fbf15c01
masataka_iMac <cdehilmnrr@gmail.com>
2019-08-10 15:04:53 +0900

    not nullをissetで肯定形で表現

--- app/Http/Middleware/JapaneseTranslationMiddleware.php
+++ app/Http/Middleware/JapaneseTranslationMiddleware.php
@@ -18,7 +18,7 @@ class JapaneseTranslationMiddleware
         //$request->activity_placeを和訳
         $activity_place_En = $request->activity_place;
         $activity_place_Ja = null;
-        if ($activity_place_En != null) {
+        if (isset($activity_place_En)) {
             $activity_place = str_replace('tokyo', '東京', $activity_place_En);
             $activity_place = str_replace('osaka', '大阪', $activity_place);
             $activity_place = str_replace('fukuoka', '福岡', $activity_place);
@@ -29,7 +29,7 @@ class JapaneseTranslationMiddleware
         //$request->genreを和訳
         $genre_En = $request->genre;
         $genre_Ja = null;
-        if ($genre_En != null) {
+        if (isset($genre_En)) {
             $genre = str_replace('manzai', '漫才', $genre_En);
             $genre = str_replace('konto', 'コント', $genre);
             $genre_Ja = str_replace('both', '両方', $genre);
@@ -37,7 +37,7 @@ class JapaneseTranslationMiddleware
         //$request->roleを和訳
         $role_En = $request->role;
         $role_Ja = null;
-        if ($role_En != null) {
+        if (isset($role_En)) {
             $role = str_replace('boke', 'ボケ', $role_En);
             $role = str_replace('tukkomi', 'ツッコミ', $role);
             $role_Ja = str_replace('boke_tukkomi', 'こだわらない', $role);
@@ -45,7 +45,7 @@ class JapaneseTranslationMiddleware
         //$request->createrを和訳
         $creater_En = $request->creater;
         $creater_Ja = null;
-        if ($creater_En != null) {
+        if (isset($creater_En)) {
             $creater = str_replace('me', '自分が作る', $creater_En);
             $creater = str_replace('together', '一緒に作りたい', $creater);
             $creater_Ja = str_replace('you', '相方に作ってほしい', $creater);
@@ -53,7 +53,7 @@ class JapaneseTranslationMiddleware
         //$request->targetを和訳
         $target_En = $request->target;
         $target_Ja = null;
-        if ($target_En != null) {
+        if (isset($target_En)) {
             $target = str_replace('golden', 'ゴールデンで冠番組を持つ', $target_En);
             $target = str_replace('midnight', '深夜で面白い番組がしたい', $target);
             $target_Ja = str_replace('theater', 'テレビより舞台で活躍したい', $target);
